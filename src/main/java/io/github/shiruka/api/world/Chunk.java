7e447a0156698c00b33a722703123876b378b565
Hasan Demirtaş <utsukushihito@outlook.com>
2020-11-30 00:27:48 +0300

    add chunk.

---
+++ src/main/java/io/github/shiruka/api/world/Chunk.java
@@ -0,0 +1,46 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Shiru ka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package io.github.shiruka.api.world;
+
+/**
+ * a class that represents a Minecraft chunk, a 16x16 section of the world which is individually loaded as needed.
+ */
+public interface Chunk {
+
+  /**
+   * obtains the x coordinate at which this chunk is located.
+   *
+   * @return the x coordinate.
+   */
+  int getX();
+
+  /**
+   * obtains the z coordinate at which this chunk is located.
+   *
+   * @return the z coordinate.
+   */
+  int getZ();
+}
