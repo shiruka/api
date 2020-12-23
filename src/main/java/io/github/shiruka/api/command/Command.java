bec4c3d5348588e4f2a2b2c2276d43b9e94e9c57
Hasan Demirtaş <utsukushihito@outlook.com>
2020-12-23 20:53:17 +0300

    Update Command.java

--- src/main/java/io/github/shiruka/api/command/Command.java
+++ src/main/java/io/github/shiruka/api/command/Command.java
@@ -26,20 +26,13 @@
 package io.github.shiruka.api.command;
 
 import io.github.shiruka.api.base.Named;
-import org.jetbrains.annotations.NotNull;
+import java.util.function.BiConsumer;
 
 /**
  * an interface to determine commands.
  *
  * @param <V> type of the current arg.
  */
-public interface Command<V> extends Named {
+public interface Command<V> extends BiConsumer<CommandSender, ArgCollection<V>>, Named {
 
-  /**
-   * runs the command with the given parameters.
-   *
-   * @param sender the sender to run.
-   * @param collection the arg collection to run.
-   */
-  void run(@NotNull CommandSender sender, @NotNull ArgCollection<V> collection);
 }
