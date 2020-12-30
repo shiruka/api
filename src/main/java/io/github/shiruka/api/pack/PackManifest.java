86c6ef83398d94137e1a52e362986efcbccabd1a
Hasan Demirtaş <utsukushihito@outlook.com>
2020-12-30 17:34:57 +0300

    move

--- src/main/java/io/github/shiruka/api/pack/PackManifest.java
+++ src/main/java/io/github/shiruka/api/pack/PackManifest.java
@@ -184,6 +184,44 @@ public final class PackManifest {
     return this.subPacks;
   }
 
+  /**
+   * an enum class that represents resource pack types.
+   */
+  public enum PackType {
+    /**
+     * the invalid.
+     */
+    INVALID,
+    /**
+     * the resources.
+     */
+    RESOURCES,
+    /**
+     * the data.
+     */
+    DATA,
+    /**
+     * the plugin.
+     */
+    PLUGIN,
+    /**
+     * the client data.
+     */
+    CLIENT_DATA,
+    /**
+     * the interface.
+     */
+    INTERFACE,
+    /**
+     * the mandatory.
+     */
+    MANDATORY,
+    /**
+     * the world template.
+     */
+    WORLD_TEMPLATE
+  }
+
   public static final class Dependency {
 
     @NotNull
