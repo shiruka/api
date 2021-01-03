211514d60cbcd29317ec311935a2def8f1fa1499
Hasan Demirtaş <utsukushihito@outlook.com>
2021-01-04 01:10:58 +0300

    change logs. (#126)

--- src/main/java/io/github/shiruka/api/plugin/PluginLoadOrder.java
+++ src/main/java/io/github/shiruka/api/plugin/PluginLoadOrder.java
@@ -35,8 +35,7 @@ public enum PluginLoadOrder {
    */
   STARTUP,
   /**
-   * indicates that the plugin will be loaded after the first/default world
-   * was created.
+   * indicates that the plugin will be loaded after the first/default world was created.
    */
   POST_WORLD
 }
