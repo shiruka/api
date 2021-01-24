3bfc570062d16a4cf6fa5e36d70bd2765ab10e36
Hasan Demirtaş <utsukushihito@outlook.com>
2021-01-24 22:16:28 +0300

    to the new one. (#201)

--- src/test/java/net/shiruka/api/MockServer.java
+++ src/test/java/net/shiruka/api/MockServer.java
@@ -74,7 +74,7 @@ public final class MockServer implements Server {
   @Override
   public ServerDescription getServerDescription(final boolean forceUpdate) {
     return new ServerDescription(
-      GameMode.SURVIVAL, 0, 0, 0, 0, 0L, "", "", ServerDescription.Edition.MCPE, new String[0], 0, "");
+      GameMode.SURVIVAL, 0, 0, 0, "", "", ServerDescription.Edition.MCPE, 0, "");
   }
 
   @Override
