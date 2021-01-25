6a9beec95133da33b656f9ccadb185617fc6bfb4
Hasan Demirtaş <utsukushihito@outlook.com>
2021-01-25 18:30:52 +0300

    Update EventHandler.java

--- src/main/java/net/shiruka/api/event/EventHandler.java
+++ src/main/java/net/shiruka/api/event/EventHandler.java
@@ -39,7 +39,7 @@ import org.jetbrains.annotations.NotNull;
 public @interface EventHandler {
 
   /**
-   * should not receive events even if they have been {@link Cancellable#cancelled()}.
+   * should not receive events even if they have been {@link Cancellable#isCancelled()}.
    *
    * @return {@code true} if the event ignores being cancelled.
    */
