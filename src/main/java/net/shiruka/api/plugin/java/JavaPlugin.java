a3d5173bd71fe2fd61e3e3b7cafab31aefe698d4
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-31 11:40:05 +0300

    remove licenses.

--- src/main/java/net/shiruka/api/plugin/java/JavaPlugin.java
+++ src/main/java/net/shiruka/api/plugin/java/JavaPlugin.java
@@ -1,31 +1,5 @@
-/*
- * MIT License
- *
- * Copyright (c) 2021 Shiru ka
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- */
-
 package net.shiruka.api.plugin.java;
 
-import io.github.portlek.configs.configuration.FileConfiguration;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Objects;
@@ -44,22 +18,10 @@ import org.jetbrains.annotations.Nullable;
 public abstract class JavaPlugin implements Plugin {
 
   /**
-   * the default YAML configuration of the plugin.
-   */
-  @Nullable
-  private FileConfiguration config;
-
-  /**
    * the logger.
    */
   @Nullable
   private Logger logger;
-
-  @NotNull
-  @Override
-  public final FileConfiguration getConfig() {
-    return Objects.requireNonNull(this.config, "The plugin not initiated yet!");
-  }
 
   @NotNull
   @Override
