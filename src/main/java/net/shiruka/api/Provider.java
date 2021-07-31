9184321ae0566d128113343b3af6ec3e0a8ddf09
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-31 18:00:20 +0300

    implement provider with inner class.

--- src/main/java/net/shiruka/api/Provider.java
+++ src/main/java/net/shiruka/api/Provider.java
@@ -1,13 +1,27 @@
 package net.shiruka.api;
 
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * an interface to determine interface providers.
  */
 public interface Provider {
+
+  /**
+   * creates a simple provider.
+   *
+   * @return a newly created simple provider.
+   */
+  @NotNull
+  static Provider create() {
+    return new Impl();
+  }
 
   /**
    * provides the given class's implementation.
@@ -41,4 +55,29 @@ public interface Provider {
    * @param <T> type of the provided object.
    */
   <T> void register(@NotNull T t);
+
+  /**
+   * a simple implementation for {@link Provider}.
+   */
+  @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+  final class Impl implements Provider {
+
+    /**
+     * the implementations.
+     */
+    @NotNull
+    private final Map<Class<?>, Object> implementations = new ConcurrentHashMap<>();
+
+    @NotNull
+    @Override
+    public <T> Optional<T> provide(@NotNull final Class<? extends T> cls) {
+      //noinspection unchecked
+      return Optional.ofNullable((T) this.implementations.get(cls));
+    }
+
+    @Override
+    public <T> void register(@NotNull final T t) {
+      this.implementations.put(t.getClass(), t);
+    }
+  }
 }
