14280207b0ea8dda840c9556261318395b88b991
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-14 16:58:48 +0300

    Revert "Update PluginClassLoader.java"
    This reverts commit 89b97934b644be8cb1ac4134489e1c9b0551d10c.

--- src/main/java/io/github/shiruka/api/plugin/java/PluginClassLoader.java
+++ src/main/java/io/github/shiruka/api/plugin/java/PluginClassLoader.java
@@ -84,7 +84,6 @@ public final class PluginClassLoader extends URLClassLoader {
    *
    * @throws InvalidPluginException if something goes wrong when creating the plugin's instance.
    * @throws IOException if an I/O error has occurred.
-   * @todo #401:15m Move the codes into a static method called from.
    */
   PluginClassLoader(@NotNull final JavaPluginLoader loader, @Nullable final ClassLoader parent,
                     @NotNull final Plugin.Description description, @NotNull final File dataFolder,
