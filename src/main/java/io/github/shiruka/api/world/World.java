db495fc513bee0f83e4580710b28a83de2353a02
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-14 15:50:18 +0300

    add difficulty and, game rule.

--- src/main/java/io/github/shiruka/api/world/World.java
+++ src/main/java/io/github/shiruka/api/world/World.java
@@ -1,6 +1,7 @@
 package io.github.shiruka.api.world;
 
 import io.github.shiruka.api.base.Location;
+import java.util.Collection;
 import java.util.UUID;
 import org.jetbrains.annotations.NotNull;
 
@@ -8,6 +9,48 @@ import org.jetbrains.annotations.NotNull;
  * an interface to determine Minecraft worlds.
  */
 public interface World {
+
+  /**
+   * obtains the difficulty.
+   *
+   * @return difficulty.
+   */
+  @NotNull
+  Difficulty difficulty();
+
+  /**
+   * sets the difficulty.
+   *
+   * @param difficulty the difficulty to set.
+   */
+  void difficulty(@NotNull Difficulty difficulty);
+
+  /**
+   * gets the game rule.
+   *
+   * @param gameRule the game rule to get.
+   * @param <T> type of the game rule.
+   *
+   * @return game rule's value.
+   */
+  @NotNull <T> T gameRule(@NotNull GameRule<T> gameRule);
+
+  /**
+   * sets the game rule's value.
+   *
+   * @param gameRule the game rule to set.
+   * @param value the value to set.
+   * @param <T> type of the value.
+   */
+  <T> void gameRule(@NotNull GameRule<T> gameRule, @NotNull T value);
+
+  /**
+   * obtains the all game rules.
+   *
+   * @return all game rules.
+   */
+  @NotNull
+  Collection<GameRule<?>> gameRules();
 
   /**
    * obtains the name.
