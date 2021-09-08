4621c13a9344ddafb84739c7a393c71d78705ebf
Hasan Demirtaş <utsukushihito@outlook.com>
2021-09-09 01:28:46 +0300

    Add status for scheduled tasks.

--- src/main/java/io/github/shiruka/api/scheduler/ScheduledTask.java
+++ src/main/java/io/github/shiruka/api/scheduler/ScheduledTask.java
@@ -1,5 +1,7 @@
 package io.github.shiruka.api.scheduler;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * an interface to determine scheduled tasks.
  */
@@ -9,4 +11,30 @@ public interface ScheduledTask {
    * cancels the task.
    */
   void cancel();
+
+  /**
+   * obtains the status.
+   *
+   * @return status.
+   */
+  @NotNull
+  Status status();
+
+  /**
+   * an enum class that contains status of scheduled tasks.
+   */
+  enum Status {
+    /**
+     * the running.
+     */
+    RUNNING,
+    /**
+     * the cancelled.
+     */
+    CANCELLED,
+    /**
+     * the finished.
+     */
+    FINISHED
+  }
 }
